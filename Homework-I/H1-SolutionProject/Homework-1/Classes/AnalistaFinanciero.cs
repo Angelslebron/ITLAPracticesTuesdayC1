da9753bce3771b110500e9f2d680f224f08db00d
Angelslebron <angellebronq17@gmail.com>
2026-02-08 21:20:36 -0400

    Uploading Homework-I.

---
+++ Homework-I/H1-SolutionProject/Homework-1/Classes/AnalistaFinanciero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1.Classes
+{
+    internal class AnalistaFinanciero:EmpleadoFinanzas
+    {
+        public int ProyectosAsignados;
+        public required string NivelExperiencia;
+
+        public void AnalizarDatos()
+        {
+            
+        }
+
+        public string GenerarReporte(string ReporteGenerado)
+        { ReporteGenerado = "Reporte generado por el Analista Financiero: " + ReporteGenerado;
+
+            return ReporteGenerado; 
+        }
+    }
+}
---
+++ Homework-I/H1-SolutionProject/Homework-1/Classes/GerenteTI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1.Classes
+{
+    internal class GerenteTI : EmpleadoTI
+    {
+        public int ProyectosTI;
+        public int EquipoDesarrollo;
+        public decimal BonoTecnico;
+
+        public void AsignarProyecto()
+        { }
+
+        public decimal CalcularSalarioTotal()
+        {
+            // Suma el salario base y el bono técnico para el gerente TI
+            return SalarioBase + BonoTecnico;
+        }
+    }
+}
