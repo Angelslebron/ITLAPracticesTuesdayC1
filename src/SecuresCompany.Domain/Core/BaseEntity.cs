498cf182af8d185375ccb0f52f065c4ff9983f9c
Angelslebron <angellebronq17@gmail.com>
2026-03-21 19:11:36 -0400

    Revert "8 implementation of domain & infrastructure layers"

--- src/SecuresCompany.Domain/Core/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SecuresCompany.Domain.Core;
-
-public abstract class BaseEntity
-{
-    public int Id { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public DateTime? UpdatedAt { get; set; }
-    public bool IsActive { get; set; } = true;
-}
