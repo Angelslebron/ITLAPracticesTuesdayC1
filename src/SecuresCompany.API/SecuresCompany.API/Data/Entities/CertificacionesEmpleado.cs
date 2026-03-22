8d58e8ccd13c6d2a640ebb299daef2ee475d00b4
Angelslebron <angellebronq17@gmail.com>
2026-03-21 20:34:40 -0400

    Reapply "Merge pull request #12 from Angelslebron/revert-10-revert-9-8-implementation-of-domain-&-infrastructure-layers"
    This reverts commit 40bbeadd7d8e1ddc775dd6a6ff661ec57eff9272.

---
+++ src/SecuresCompany.API/SecuresCompany.API/Data/Entities/CertificacionesEmpleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecuresCompany.API.Data.Entities;
+
+public partial class CertificacionesEmpleado
+{
+    public int CertificacionId { get; set; }
+
+    public int EmpleadoId { get; set; }
+
+    public string NombreCertificacion { get; set; } = null!;
+
+    public DateOnly? FechaObtencion { get; set; }
+
+    public virtual Empleado Empleado { get; set; } = null!;
+}
